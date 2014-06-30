f4f494b31d7da7d4496669c0406a770e0145aa3b
Francois Rejete <francois@rejete.com>
2014-06-30 09:14:14 +0900

    scene keywords to try to extract the movie title from noise

--- digest.py
+++ digest.py
@@ -2,14 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+QUERY = 'movies added:1d'
+
 WHITELIST = ['720p', '1080p']
 BLACKLIST = ['cam', 'hdcam']
 
-payload = {'f':'movies added:1d'}
+# Scene keywords to try to identify the movie title
+KEYWORDS = [
+	'brrip',
+	'x264',
+	'dvdscr',
+	'dual audio',
+	'hevc',
+	'[0-9]{3,4}p',
+	'[0-9]{3,}mb'
+]
+
+# Get the search results
+payload = {'f':QUERY}
 r = requests.get('http://torrentz.eu/verified', params=payload)
 
+# Parse the html page
 soup = BeautifulSoup(r.text)
 
+# Find each result hit on the page and get the info
 for hit in soup.select('.results dl'):
 	name = hit.dt.a.string
 	url = 'http://torrentz.eu/' + hit.dt.a.get('href')
